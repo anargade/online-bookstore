e3fd8d91a501089d5ec609d9fe7ff9369f8da125
vinodphuke <vinodphuke@john>
2020-04-02 12:37:29 +0530

    Committed the angular chages for feature 1.

---
+++ angular-bookstore/src/app/services/book.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Book } from '../common/book';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  private baseUrl = 'http://localhost:8080/api/v1/books';
+  constructor(private httpClient:HttpClient) { }
+
+  getBooks():Observable<Book[]>{
+    return this.httpClient.get<getResponseBooks>(this.baseUrl).pipe(
+      map(response => response._embedded.books)
+    );
+  }
+}
+
+interface getResponseBooks{
+  _embedded: {
+    books: Book[];
+  }
+}
--- onlinebookstore/src/main/java/com/ashkalp/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/ashkalp/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,10 @@
 package com.ashkalp.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ashkalp.onlinebookstore.entity.Book;
-
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
